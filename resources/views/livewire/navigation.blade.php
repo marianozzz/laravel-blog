2a6054e0786ac3a61a932437c7eba959d55c1e8b
marianozzz <mariano.zanier@gmail.com>
2021-03-11 15:27:52 -0300

    correccion navigation

--- resources/views/livewire/navigation.blade.php
+++ resources/views/livewire/navigation.blade.php
@@ -6,7 +6,7 @@
             <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 <button x-on:click="open = true" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
                     aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
+                    <span class="sr-only">Abrir menu principal</span>
           
                     <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor" aria-hidden="true">
@@ -42,13 +42,14 @@
                     </div>
                 </div>
             </div>
-            
+
             @auth
                 <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-                {{--Boton notificacion--}}
+{{--Boton notificacion--}}
+
                     <button class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                        <span class="sr-only">View notifications</span>
-                        <!-- Heroicon name: bell -->
+                    <span class="sr-only">View notifications</span>
+<!-- Heroicon name: bell -->
                         <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                             stroke="currentColor" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -56,7 +57,7 @@
                         </svg>
                     </button>
 
-                    <!-- Profile dropdown -->
+ <!-- Profile dropdown -->
                     <div class="ml-3 relative" x-data="{ open: false }">
                         <div>
                             <button x-on:click="open = true" class="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white" id="user-menu" aria-haspopup="true">
@@ -72,20 +73,18 @@
                             @can('admin.home')
                             <a href="{{route('admin.home')}}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem">Dashboard</a>
                             @endcan
-                           
-                           
                             <form method="POST" action="{{ route('logout') }}">
                                 @csrf
                             <a href="{{ route('logout') }}" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100" role="menuitem" onclick="event.preventDefault(); this.closest('form').submit();">Sign out</a>
                             </form>
-                        
                         </div>
                     </div>
                 </div>  
             @else
                 <div>
-                    <a href="{{route('login')}}"    class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-sm font-medium">Login</a>
-                    <a href="{{route('register')}}" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-sm font-medium">Registro</a>
+                    <a href="{{route('login')}}"    class="text-gray-300 hover:bg-gray-700 hover:text-white  px-3 py-2 rounded-md text-sm font-medium">Login</a>
+                    <a href="{{route('register')}}" class="text-gray-300 hover:bg-gray-700 hover:text-white  px-3 py-2 rounded-md text-sm font-medium">Registro</a>
+                    
                 </div>
             @endauth
         </div>
