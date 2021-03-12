e2e0ebae6c7dcee1506e836f7c2e088de212c956
marianozzz <mariano.zanier@gmail.com>
2021-03-12 00:04:49 -0300

    Procfile

--- config/database.php
+++ config/database.php
@@ -2,8 +2,6 @@
 
 use Illuminate\Support\Str;
 
-
-$DATABASE_URL=parse_url('postgres://oheboqnrhskbta:a3f52821999cdfcf060563443ba600952d71b681529739d659dcc3c7712786cc@ec2-54-162-119-125.compute-1.amazonaws.com:5432/da1kkidv8nmeus');
 return [
 
     /*
@@ -67,15 +65,17 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => $DATABASE_URL["host"],
-            'port' => $DATABASE_URL["port"],
-            'database' => ltrim($DATABASE_URL["path"], "/"),
-            'username' => $DATABASE_URL["user"],
-            'password' => $DATABASE_URL["pass"],
+            'url' => env('DATABASE_URL'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '5432'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'charset' => 'utf8',
             'prefix' => '',
+            'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'require',
+            'sslmode' => 'prefer',
         ],
 
         'sqlsrv' => [
